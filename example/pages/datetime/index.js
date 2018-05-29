e07aefd5d1f6a236edc25751f88a606955521853
宋冬红 <249359986@qq.com>
2018-05-29 11:47:21 +0800

    去掉多余内容

--- example/pages/datetime/index.js
+++
@@ -1,8 +0,0 @@
-Page({
-  customChange ({detail}) {
-    this.setData({pickerView1: detail.value.join('-')})
-  },
-  nativeChange ({detail}) {
-    this.setData({pickerView2: detail.value.join('-')})
-  }
-})
